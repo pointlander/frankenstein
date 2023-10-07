3ee2dacc38dd793511448d136a3c7a18c6e8fa82
Andrew Snodgrass <git@pointlander.info>
2023-10-06 23:06:19 -0500

    Added data loading

--- main.go
+++ main.go
@@ -4,6 +4,64 @@
 
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
+var (
+	English = make([]string, 0, 1024)
+	German  = make([]string, 0, 1024)
+)
+
+// Load data from files
+func Load() int {
+	in, err := os.Open("train.en")
+	if err != nil {
+		panic(err)
+	}
+	defer in.Close()
+
+	reader := bufio.NewReader(in)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+		line = strings.TrimSpace(line)
+		English = append(English, line)
+	}
+
+	in, err = os.Open("train.de")
+	if err != nil {
+		panic(err)
+	}
+	defer in.Close()
+
+	reader = bufio.NewReader(in)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+		line = strings.TrimSpace(line)
+		German = append(German, line)
+	}
+
+	if len(English) != len(German) {
+		panic("English and German data size not equal")
+	}
+	return len(English)
+}
+
+func main() {
+	size := Load()
+	fmt.Println("size: ", size)
 }
